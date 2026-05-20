179f1f42b1b25499b9dcc7e57c38c19a9a93da7f
Amedu Ehis <ameduosas16@gmail.com>
2026-05-20 14:54:27 +0100

    minimum function

--- hello.js
+++ hello.js
@@ -1,5 +1,23 @@
 console.log("Hello World");
 const a = 10;
 const b = 20;
- //return value
+//returns value
 console.log(a + b);
+
+function mult(x, y) {
+    return x * y;
+}
+console.log(mult(10, 20)); 
+
+function power(b,p){
+  return b ** p;
+}
+
+console.log(power(2,3));  
+
+// let result = Math.min(a, b);
+// console.log(result);
+
+//Custom function (Arrow syntax)
+const findMin = (a, b) => a < b ? a : b;
+console.log(findMin(10, 20));  
