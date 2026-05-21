8267b1a7fe9bece4ac19526a3b7850e93a1d68e9
Amedu Ehis <ameduosas16@gmail.com>
2026-05-21 14:25:37 +0100

    testing different JS functionalities

--- hello.js
+++ hello.js
@@ -1,3 +1,5 @@
+import { name } from "ejs";
+
 console.log("Hello World");
 const a = 10;
 const b = 20;
@@ -21,3 +23,23 @@ console.log(power(2,3));
 //Custom function (Arrow syntax)
 const findMin = (a, b) => a < b ? a : b;
 console.log(findMin(10, 20));  
+
+
+//Write a js code to reserve your name and print the reversed name
+const name = "Ehichioya";
+const reversedName = name.split("").reverse().join("");
+console.log(reversedName);
+
+//write a js code to change the value of an array  
+const arr = [1, 2, 3, 4, 5];
+arr.push(6);
+console.log(arr);
+
+//write a js code to find the factorial of a number using the concept of recursion
+function factorial(n){
+  if(n === 0){
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+console.log(factorial(5));
